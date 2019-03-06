378934c4cab345e17d50c30929c97620b4aa348d
SaraHdzGarcia <301610199@bis.ute.edu.mx>
2019-03-05 18:46:29 -0600

    LoginController

--- TiendaVirtual/ShoesShop/Controllers/LoginController.cs
+++ TiendaVirtual/ShoesShop/Controllers/LoginController.cs
@@ -12,34 +12,34 @@ namespace ShoesShop.Controllers
     public class LoginController : Controller
     {
         // GET: Login
-        public ActionResult Login(UserModel usm)
+        public ActionResult Login(UserModel usm,string user, string pass)
         {
             if (ModelState.IsValid)
             {
                 using (DbContextShop dbCtx = new DbContextShop())
                 {
-                    int rows = dbCtx.Usersses.Count();
-                    if (rows == 0)
-                    {
-                        Userss usem = new Userss()
-                        {
-                            LastName = "Luna Rojo",
-                            FirstName = "Yesenia",
-                            Address="Bejuco",
-                            ExtNumber="123",
-                            City="Escobedo",
-                            PostalCode="66063",
-                            Country="Mexico",
-                            Telephone = "8110828500",
-                            UserName = "YeseniaM",
-                            Password = EncryptionDecryption.EncriptarSHA1("MadaiLR"),
-                        };
-                        dbCtx.Usersses.Add(usem);
-                        dbCtx.SaveChanges();
-                    }
-                    string encryptedPass = EncryptionDecryption.EncriptarSHA1(usm.Password);
+                    //int rows = dbCtx.Usersses.Count();
+                    //if (rows >= 0)
+                    //{
+                    //    Userss usem = new Userss()
+                    //    {
+                    //        LastName = "Hernandez Garcia",
+                    //        FirstName = "Sara",
+                    //        Address="Av. La Unidad",
+                    //        ExtNumber="539",
+                    //        City="Escobedo",
+                    //        PostalCode="64060",
+                    //        Country="Mexico",
+                    //        Telephone = "8124913721",
+                    //        UserName = "SaraNhm",
+                    //        Password = EncryptionDecryption.EncriptarSHA1("Sobrinos5"),
+                    //    };
+                    //    dbCtx.Usersses.Add(usem);
+                    //    dbCtx.SaveChanges();
+                //}
+                string encryptedPass = EncryptionDecryption.EncriptarSHA1(usm.Password);
 
-                    var isLogged = dbCtx.Usersses
+                var isLogged = dbCtx.Usersses
                         .Where(x => x.UserName.Equals(usm.UserName)
                         && x.Password.Equals(encryptedPass))
                         .FirstOrDefault();
