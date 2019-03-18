5c666154d2b7ceacaaa6e8834a01a59dd953c98c
SaraHdzGarcia <301610199@bis.ute.edu.mx>
2019-03-18 14:29:07 -0600

    Vista Caballero

--- TiendaVirtual/ShoesShop/Controllers/IndexController.cs
+++ TiendaVirtual/ShoesShop/Controllers/IndexController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShoesShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,18 @@ namespace ShoesShop.Controllers
 {
     public class IndexController : Controller
     {
+        private static DbContextShop dbCtx = new DbContextShop();
+
         // GET: Index
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        public ActionResult Caballero()
+        {
+            return View(dbCtx.Products.ToList().Where(x=>x.CategoryID==1).OrderBy(x=>x.BarCode));
         }
     }
 }
